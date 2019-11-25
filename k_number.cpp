39dc0733a35234d08003b3c3949d644fff11387e
Kim-da-young <06dyk26@naver.com>
2019-11-25 19:33:07 +0900

    Update k_number.cpp

--- k_number.cpp
+++ k_number.cpp
@@ -1,10 +1,43 @@
-
+// 프로그래머스 (K번째수)
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <iostream>
 
 using namespace std;
+vector<int> solution(vector<int> array, vector<vector<int>> commands);
+
+int main()
+{
+	int i, arr_num, com_num;
+	cout << "배열 크기 : ";
+	cin >> arr_num;
+	vector<int> array(arr_num);
+	cout << "배열 array" << endl;
+	for (i = 0; i < arr_num; i++)
+	{
+		cin >> array[i];
+	}
+	cout << "명령 수 : " << endl;
+	cin >> com_num;
+	vector<vector<int>> commands(com_num, vector<int>(3));
+	for (i = 0; i < com_num; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> commands[i][j];
+		}
+	}
+	vector<int> answer(com_num);
+	answer = solution(array, commands);
+	cout << "각 명령의 k번째 수" <<endl;
+	for (i = 0; i < com_num; i++)
+	{
+		cout << answer[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 	vector<int> answer;
@@ -17,13 +50,4 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 		tmp.clear();
 	}
 	return answer;
-}
-int main()
-{
-	vector<int> array = {1,5,2,6,3,7,4};
-	vector<vector<int>> commands = { {2,5,3},{4,4,1},{1,7,3} };
-	vector<int> answer = solution(array, commands);
-	for(int i = 0;i<answer.size();i++)
-		cout << answer[i] << endl;
-	return 0;
 }
